3649390443845d0596732754e1bc0b3c7478653c
Sridhar.Bandi <sridhar.bandi.ece@gmail.com>
2018-12-14 13:18:00 -0800

    Handler Tp Run Accessibility

--- src/main/java/com/accessibility/handler/Handler.java
+++ src/main/java/com/accessibility/handler/Handler.java
@@ -1,5 +1,17 @@
 package com.accessibility.handler;
 
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+
 public class Handler {
 
+    private WebDriver driver;
+
+    public Handler(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public void runAccessibility(){
+        JavascriptExecutor javascriptExecutor =  (JavascriptExecutor) driver;
+    }
 }
