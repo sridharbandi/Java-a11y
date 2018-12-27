28d226c35174afc76b8687940213d5709763e59b
Sridhar.Bandi <sridhar.bandi.ece@gmail.com>
2018-12-27 13:46:21 -0800

    Moved Chart Script to separate file

---
+++ src/main/resources/js/main.js
@@ -0,0 +1,40 @@
+var ctx = document.getElementById("myChart").getContext('2d');
+var barChartData = {
+    labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+    datasets: [{
+        label: 'Errors',
+        backgroundColor: '#dc3545',
+        data: [18, 46, 49, 20, 56, 88, 66]
+    }, {
+        label: 'Warnings',
+        backgroundColor: '#ffc107',
+        data: [66, 16, 39, 70, 96, 28, 89]
+    }, {
+        label: 'Notices',
+        backgroundColor: '#17a2b8',
+        data: [18, 96, 19, 80, 36, 68, 16]
+    }]
+
+};
+var myChart = new Chart(ctx, {
+    type: 'bar',
+    data: barChartData,
+    options: {
+        title: {
+            display: true,
+            text: 'URL Statistics'
+        },
+        tooltips: {
+            mode: 'index',
+            intersect: false
+        },
+        scales: {
+            xAxes: [{
+                stacked: true
+            }],
+            yAxes: [{
+                stacked: true
+            }]
+        }
+    }
+});
