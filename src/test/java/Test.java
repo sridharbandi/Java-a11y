695899a24f5f51dd9edce3965ffdf07d7739e142
Sridhar.Bandi <sridhar.bandi.ece@gmail.com>
2019-01-18 11:22:25 -0800

    Updated Test

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -1,5 +1,6 @@
 import com.a11y.accessibility.AccessibilityRunner;
 import io.github.bonigarcia.wdm.ChromeDriverManager;
+import org.junit.After;
 import org.junit.Before;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -10,52 +11,38 @@ import org.openqa.selenium.logging.LogType;
 import org.openqa.selenium.logging.LoggingPreferences;
 import org.openqa.selenium.remote.CapabilityType;
 
-import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 import java.util.logging.Level;
 
 public class Test {
     private WebDriver driver;
-   // private Handler handler;
     private AccessibilityRunner accessibilityRunner;
 
     @Before
-    public void beforeTest() throws IOException {
+    public void beforeTest() {
         ChromeDriverManager.chromedriver().setup();
         ChromeOptions chromeOptions = new ChromeOptions();
         LoggingPreferences logPrefs = new LoggingPreferences();
         logPrefs.enable(LogType.BROWSER, Level.ALL);
         chromeOptions.setCapability(CapabilityType.LOGGING_PREFS, logPrefs);
-         driver = new ChromeDriver(chromeOptions);
-       /* ChromeDriverManager.chromedriver().version("2.43").setup();
-        ChromeOptions chromeOptions = new ChromeOptions();
-        LoggingPreferences logPrefs = new LoggingPreferences();
-        logPrefs.enable(LogType.BROWSER, Level.ALL);
-        chromeOptions.setCapability(CapabilityType.LOGGING_PREFS, logPrefs);
-        driver = new ChromeDriver(chromeOptions);*/
-        //handler = new Handler(driver);
+        driver = new ChromeDriver(chromeOptions);
         accessibilityRunner = new AccessibilityRunner(driver);
-
     }
 
-    /*@After
-    public void tearDown(){
+    @After
+    public void tearDown() {
         driver.quit();
-    }*/
+    }
 
     @org.junit.Test
-    public void runSnifferTest() throws IOException, InterruptedException {
+    public void runSnifferTest() throws InterruptedException {
         driver.get("https://www.google.co.uk/");
         driver.manage().timeouts().pageLoadTimeout(120, TimeUnit.SECONDS);
-       // handler.runAccessibility("SS");
         accessibilityRunner.execute("Google");
         WebElement searchBox = driver.findElement(By.name("q"));
         searchBox.sendKeys("ChromeDriver");
         searchBox.submit();
-        Thread.sleep(5000);
         accessibilityRunner.execute();
         accessibilityRunner.generateHtmlReport();
     }
-
-
 }
