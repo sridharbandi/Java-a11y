b0b23e54a298759cab577ca447c3e4416ea4e9f5
Sridhar.Bandi <sridhar.bandi.ece@gmail.com>
2019-01-20 10:16:04 -0800

    Updated Tests

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -1,10 +1,9 @@
 import com.a11y.accessibility.AccessibilityRunner;
 import io.github.bonigarcia.wdm.ChromeDriverManager;
 import org.junit.After;
+import org.junit.AfterClass;
 import org.junit.Before;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.logging.LogType;
@@ -16,7 +15,7 @@ import java.util.logging.Level;
 
 public class Test {
     private WebDriver driver;
-    private AccessibilityRunner accessibilityRunner;
+    private static AccessibilityRunner accessibilityRunner;
 
     @Before
     public void beforeTest() {
@@ -26,6 +25,7 @@ public class Test {
         logPrefs.enable(LogType.BROWSER, Level.ALL);
         chromeOptions.setCapability(CapabilityType.LOGGING_PREFS, logPrefs);
         driver = new ChromeDriver(chromeOptions);
+        driver.manage().timeouts().pageLoadTimeout(120, TimeUnit.SECONDS);
         accessibilityRunner = new AccessibilityRunner(driver);
     }
 
@@ -34,15 +34,20 @@ public class Test {
         driver.quit();
     }
 
-    @org.junit.Test
-    public void runSnifferTest() throws InterruptedException {
-        driver.get("https://www.google.co.uk/");
-        driver.manage().timeouts().pageLoadTimeout(120, TimeUnit.SECONDS);
-        accessibilityRunner.execute("Google");
-        WebElement searchBox = driver.findElement(By.name("q"));
-        searchBox.sendKeys("ChromeDriver");
-        searchBox.submit();
-        accessibilityRunner.execute();
+    @AfterClass
+    public static void generateReport(){
         accessibilityRunner.generateHtmlReport();
+    }
+
+
+    @org.junit.Test
+    public void googleTest() throws InterruptedException {
+        driver.get("https://www.google.co.uk/");
+        accessibilityRunner.execute("Google");
+    }
+    @org.junit.Test
+    public void w3cschoolsTest() throws InterruptedException {
+        driver.get("https://www.w3schools.com/");
+        accessibilityRunner.execute();
     }
 }
