28103dc0992865be4a5a72349728f9e99cf4f2e2
Sridhar Bandi <sridhar.bandi.ece@gmail.com>
2019-10-14 20:36:31 +0100

    Handled for media play

--- src/main/java/io/github/sridharbandi/report/Result.java
+++ src/main/java/io/github/sridharbandi/report/Result.java
@@ -1,16 +1,16 @@
 /**
  * Copyright (c) 2019 Sridhar Bandi.
- *
+ * <p>
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- *
+ * <p>
  * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
- *
+ * <p>
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -28,6 +28,7 @@ import org.openqa.selenium.logging.LogEntries;
 import org.openqa.selenium.logging.LogEntry;
 import org.openqa.selenium.logging.LogType;
 
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.regex.Matcher;
@@ -42,7 +43,7 @@ public class Result extends Report {
         this.driver = driver;
     }
 
-    protected List<Issue> issueList(){
+    protected List<Issue> issueList() {
         LogEntries logEntries = driver.manage().logs().get(LogType.BROWSER);
         return logEntries.getAll().stream()
                 .map(LogEntry::getMessage)
@@ -50,7 +51,7 @@ public class Result extends Report {
                 .filter(str -> !str.endsWith("\"done\""))
                 .map(str -> str.split("HTMLCS\\]")[1])
                 .map(str -> str.substring(0, str.length() - 1))
-                .map(issue ->{
+                .map(issue -> {
                     Issue _issue = new Issue();
                     String[] arrIssue = issue.split("\\|");
                     _issue.setIssueType(arrIssue[0].trim());
@@ -59,7 +60,7 @@ public class Result extends Report {
                     _issue.setIssueTag(arrIssue[2]);
                     _issue.setIssueId(arrIssue[3]);
                     _issue.setIssueMsg(arrIssue[4]);
-                    _issue.setIssueElement(StringEscapeUtils.unescapeJava(arrIssue[5]));
+                    _issue.setIssueElement(arrIssue.length < 6 ? "" : StringEscapeUtils.unescapeJava(arrIssue[5]));
                     return _issue;
                 }).collect(Collectors.toList());
     }
@@ -70,21 +71,20 @@ public class Result extends Report {
         LinkedList<String> codes = new LinkedList<>();
         while (matcher.find()) {
             String match = matcher.group();
-            if(match.contains(",")){
+            if (match.contains(",")) {
                 String[] techniques = match.split(",");
-                for (String code : techniques){
+                for (String code : techniques) {
                     codes.add(code);
                 }
-            }
-            else {
+            } else {
                 codes.add(match);
             }
         }
-        if(codes.size() != 0){
+        if (codes.size() != 0) {
             codes.remove(0);
         }
         return codes.stream()
-                .map(code -> "https://www.w3.org/TR/WCAG20-TECHS/"+code)
+                .map(code -> "https://www.w3.org/TR/WCAG20-TECHS/" + code)
                 .collect(Collectors.toList());
     }
 
