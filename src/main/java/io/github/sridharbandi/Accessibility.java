92afd4b5303c25f9dbe23a17fa6591b5855a9828
Sridhar Bandi <sridhar.bandi.ece@gmail.com>
2020-01-12 14:49:30 +0000

    Clean up

--- src/main/java/io/github/sridharbandi/Accessibility.java
+++ src/main/java/io/github/sridharbandi/Accessibility.java
@@ -1,16 +1,16 @@
 /**
  * Copyright (c) 2019 Sridhar Bandi.
- *
+ * <p>
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- *
+ * <p>
  * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
- *
+ * <p>
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -24,8 +24,7 @@ package io.github.sridharbandi;
 import io.github.sridharbandi.util.Standard;
 
 public class Accessibility {
-
-    public static String STANDARD = Standard.WCAG2AA;
-    public static String REPORT_PATH =  System.getProperty("user.dir")+"/accessibility";
+    public static Standard STANDARD = Standard.WCAG2AA;
+    public static String REPORT_PATH = System.getProperty("user.dir") + "/accessibility";
     public static boolean LOG_RESULTS = true;
 }
