61506e35e87dd4fd646701cba2a5817ec0cae7c8
Sridhar Bandi <sridhar.bandi.ece@gmail.com>
2020-01-12 14:45:27 +0000

    Changed to Enum Class

--- src/main/java/io/github/sridharbandi/util/Standard.java
+++ src/main/java/io/github/sridharbandi/util/Standard.java
@@ -21,9 +21,9 @@
  */
 package io.github.sridharbandi.util;
 
-public class Standard {
-    public static String WCAG2A = "WCAG2A";
-    public static String WCAG2AA = "WCAG2AA";
-    public static String WCAG2AAA = "WCAG2AAA";
-    public static String SECTION508 = "Section508";
+public enum Standard {
+    WCAG2A,
+    WCAG2AA,
+    WCAG2AAA,
+    SECTION508
 }
